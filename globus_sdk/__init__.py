702c82e433eda9f97e566b5de9c8b8817c85bffb
Stephen Rosen <sirosen@uchicago.edu>
2016-04-06 11:06:42 -0500

    Fix oversight, remove NexusClient in __init__.py
    This was lost during a rebase operation in which I was a touch careless.
    Fixup.

--- globus_sdk/__init__.py
+++ globus_sdk/__init__.py
@@ -1,6 +1,5 @@
 from globus_sdk.response import GlobusResponse, GlobusHTTPResponse
 from globus_sdk.transfer import TransferClient
-from globus_sdk.nexus import NexusClient
 from globus_sdk.auth import AuthClient
 
 from globus_sdk.exc import GlobusError, GlobusAPIError, TransferAPIError
