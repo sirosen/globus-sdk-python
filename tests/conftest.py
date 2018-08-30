c70d2c509b5db3dc78903f328b36b56d1c177cef
Stephen Rosen <sirosen@globus.org>
2018-08-30 19:42:47 +0000

    Add some Auth Flow functional tests
    Add several Auth Flow Manager tests, which validate the generated
    authorize_urls. Mocks API responses with `invalid_grant` errors -- full,
    working flows should also be added.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,11 @@ import pytest
 import httpretty
 
 
+# disable the use of real sockets when HTTPretty socket mocking is in place --
+# if you make a real API call, it will immediately error
+httpretty.httpretty.allow_net_connect = False
+
+
 @pytest.fixture(autouse=True)
 def enable_httpretty():
     """
