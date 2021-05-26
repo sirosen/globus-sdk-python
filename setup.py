4c67ad86054365fbf3cd4efcda8b52ee5db09616
Stephen Rosen <sirosen@globus.org>
2021-05-26 22:44:39 +0000

    Switch to a src/ layout and add py.typed
    This layout has benefits when running tests and type-checking. Because
    the current directory is always in the import path for python and it's
    typical for tox and pytest runs to take place from the repo root,
    `import globus_sdk` can easily refer to the local copy, *not* an
    installed version. This means that in the presence of a packaging
    issue or any bug which behaves differently under a package install (vs
    local imports), the local import behavior could mask the problem.
    
    Also, add a dedicated `mypy` tox env which does a `mypy` run against
    the whole `src/` directory. This helps avoid issues which can be left
    uncaught by `pre-commit` when multiple modules conflict, but
    each file is internally consistent.
    
    Add a `py.typed` file, indicating that `globus_sdk` provides type
    annotations.

--- setup.py
+++ setup.py
@@ -1,50 +1,61 @@
 import os.path
+import re
 
 from setuptools import find_packages, setup
 
-# single source of truth for package version
-version_ns = {}  # type: ignore
-with open(os.path.join("globus_sdk", "version.py")) as f:
-    exec(f.read(), version_ns)
+DEV_REQUIREMENTS = [
+    # lint
+    "flake8<4",
+    "isort<6",
+    "black==21.5b1",
+    "flake8-bugbear==21.4.3",
+    "mypy==0.812",
+    # tests
+    "pytest<7",
+    "pytest-cov<3",
+    "pytest-xdist<3",
+    "responses==0.13.3",
+    # docs
+    "sphinx<5",
+    "sphinx-material==0.0.32",
+]
+
+
+def parse_version():
+    # single source of truth for package version
+    version_string = ""
+    version_pattern = re.compile(r'__version__ = "([^"]*)"')
+    with open(os.path.join("src", "globus_sdk", "version.py")) as f:
+        for line in f:
+            match = version_pattern.match(line)
+            if match:
+                version_string = match.group(1)
+                break
+    if not version_string:
+        raise RuntimeError("Failed to parse version information")
+    return version_string
+
+
+def read_readme():
+    with open("README.rst") as fp:
+        return fp.read()
+
 
 setup(
     name="globus-sdk",
-    version=version_ns["__version__"],
+    version=parse_version(),
     description="Globus SDK for Python",
-    long_description=open("README.rst").read(),
+    long_description=read_readme(),
     author="Globus Team",
     author_email="support@globus.org",
     url="https://github.com/globus/globus-sdk-python",
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    package_data={"globus_sdk": ["py.typed"]},
     python_requires=">=3.6",
     install_requires=["requests>=2.9.2,<3.0.0", "pyjwt[crypto]>=2.0.0,<3.0.0"],
-    extras_require={
-        # the dev extra is for SDK developers only
-        "dev": [
-            # drive testing with tox
-            "tox<4",
-            # linting
-            "flake8<4",
-            "isort<6",
-            "black==21.5b1",
-            "flake8-bugbear==21.4.3",
-            "mypy==0.812",
-            # testing
-            "pytest<7",
-            "pytest-cov<3",
-            "pytest-xdist<3",
-            # mocking HTTP responses
-            "responses==0.13.3",
-            # builds + uploads to pypi
-            "twine<4",
-            "wheel==0.36.2",
-            # docs
-            "sphinx<5",
-            "sphinx-material==0.0.32",
-        ]
-    },
-    include_package_data=True,
-    keywords=["globus", "file transfer"],
+    extras_require={"dev": DEV_REQUIREMENTS},
+    keywords=["globus"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
