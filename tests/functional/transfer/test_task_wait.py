893f415ea806ac879738f06c1841da7f00fa3022
Stephen Rosen <sirosen@globus.org>
2022-01-26 19:35:13 +0000

    Add tests to exercise `TransferCLient.task_wait`
    task_wait was not being called in the testsuite. It was the single
    largest un-tested block of source.
    
    The tests are simple and cover:
    - a task_wait with invalid args (easy)
    - a task_wait which has to sleep exactly once
    - a task_wait which has to wait until giving up

---
+++ tests/functional/transfer/test_task_wait.py
@@ -0,0 +1,66 @@
+from unittest import mock
+
+import pytest
+import responses
+
+import globus_sdk
+from tests.common import get_last_request, register_api_route_fixture_file
+
+TASK1_ID = "b8872740-7edc-11ec-9f33-ed182a728dff"
+
+
+@pytest.mark.parametrize(
+    "add_kwargs",
+    [
+        {"timeout": 0, "polling_interval": 0},
+        {"timeout": 0.5, "polling_interval": 0.5},
+        {"timeout": 5, "polling_interval": 0.5},
+        {"timeout": -5, "polling_interval": 5},
+    ],
+)
+def test_task_wait_bad_args_min_wait(client, mocksleep, add_kwargs):
+    # register task mock data even though it should not be needed
+    register_api_route_fixture_file(
+        "transfer", f"/task/{TASK1_ID}", "get_task1_active.json"
+    )
+
+    with pytest.raises(globus_sdk.GlobusSDKUsageError):
+        client.task_wait(TASK1_ID, **add_kwargs)
+
+    # no requests sent, no sleep done
+    assert get_last_request() is None
+    mocksleep.assert_not_called()
+
+
+def test_task_wait_success_case(client, mocksleep):
+    # first the task will show as active, then as succeded
+    register_api_route_fixture_file(
+        "transfer", f"/task/{TASK1_ID}", "get_task1_active.json"
+    )
+    register_api_route_fixture_file(
+        "transfer", f"/task/{TASK1_ID}", "get_task1_succeeded.json"
+    )
+
+    # do the task wait, it should return true (the task completed in time)
+    result = client.task_wait(TASK1_ID, timeout=5, polling_interval=1)
+    assert result is True
+
+    # one sleep, two network calls
+    mocksleep.assert_called_once_with(1)
+    assert len(responses.calls) == 2
+
+
+def test_task_wait_unfinished_case(client, mocksleep):
+    # the task is in the active state no matter how many times we ask
+    register_api_route_fixture_file(
+        "transfer", f"/task/{TASK1_ID}", "get_task1_active.json"
+    )
+
+    # do the task wait, it should return false (the task didn't complete)
+    result = client.task_wait(TASK1_ID, timeout=5, polling_interval=1)
+    assert result is False
+
+    # a number of sleeps equal to timeout/polling_interval
+    # a number of requests equal to sleeps+1
+    mocksleep.assert_has_calls([mock.call(1) for x in range(5)])
+    assert len(responses.calls) == 6
