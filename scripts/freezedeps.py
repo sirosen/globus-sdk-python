bdbc913136e624815ad108e27a2439c366d6f8a3
Kurt McKee <contactme@kurtmckee.org>
2023-03-14 14:27:38 -0500

    Re-root where pip-compile runs from (#705)
    This aligns with how Dependabot will submit changes.

--- scripts/freezedeps.py
+++ scripts/freezedeps.py
@@ -23,7 +23,7 @@ def main():
     reqs_in_files = REQS_DIR.glob("*.in")
     print(f'running pip-compile on "{REQS_DIR}/*.in"')
     for absolute_source in reqs_in_files:
-        source = absolute_source.relative_to(REQS_DIR)
+        source = absolute_source.relative_to(REPO_ROOT)
         dest = source.with_suffix(".txt")
         print(f"{source} -> {dest}")
         subprocess.run(
@@ -39,7 +39,7 @@ def main():
                 str(dest),
             ],
             check=True,
-            cwd=REQS_DIR,
+            cwd=REPO_ROOT,
             env={"CUSTOM_COMPILE_COMMAND": "tox run -e freezedeps"},
         )
 
