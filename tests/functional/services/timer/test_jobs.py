92e303a7b54a15f548cbb891d93ff6064f674375
Stephen Rosen <sirosen@globus.org>
2023-05-02 13:34:37 -0500

    Add more tests for TimerAPIError, fix pylint
    pylint flagged an unused string (which was being used as a multiline
    comment) as such. To fix, we could just convert to a normal comment or
    disable the pylint rule, but the better approach is to get the data
    being documented there into test fixtures.
    
    Move example Timer API responses into test fixture data, and test
    against those data. These functional tests effectively validate the
    same behaviors which are captured by error parsing unit tests from
    another angle.

--- tests/functional/services/timer/test_jobs.py
+++ tests/functional/services/timer/test_jobs.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 import pytest
 
-from globus_sdk import TimerClient, TimerJob, TransferData
+from globus_sdk import TimerAPIError, TimerClient, TimerJob, TransferData
 from globus_sdk._testing import get_last_request, load_response
 from globus_sdk.config import get_service_url
 from globus_sdk.utils import slash_join
@@ -27,6 +27,16 @@ def test_get_job(timer_client):
     response = timer_client.get_job(meta["job_id"])
     assert response.http_status == 200
     assert response.data.get("job_id") == meta["job_id"]
+
+
+def test_get_job_errors(timer_client):
+    meta = load_response(timer_client.get_job, case="simple_500_error").metadata
+    with pytest.raises(TimerAPIError) as excinfo:
+        timer_client.get_job(meta["job_id"])
+    err = excinfo.value
+    assert err.http_status == 500
+    assert err.code == "ERROR"
+    assert err.message == "Request failed terribly"
 
 
 @pytest.mark.parametrize("start", [datetime.utcnow(), "2022-04-05T06:00:00"])
@@ -58,6 +68,22 @@ def test_create_job(timer_client, start, interval):
     assert req_body["callback_url"] == slash_join(
         get_service_url("actions"), "/transfer/transfer/run"
     )
+
+
+def test_create_job_validation_error(timer_client):
+    meta = load_response(timer_client.create_job, case="validation_error").metadata
+    transfer_data = TransferData(
+        source_endpoint=GO_EP1_ID, destination_endpoint=GO_EP2_ID
+    )
+    timer_job = TimerJob.from_transfer_data(transfer_data, "2022-04-05T06:00:00", 1800)
+
+    with pytest.raises(TimerAPIError) as excinfo:
+        timer_client.create_job(timer_job)
+
+    err = excinfo.value
+    assert err.http_status == 422
+    assert err.code == "Validation Error"
+    assert err.messages == meta["expect_messages"]
 
 
 def test_update_job(timer_client):
