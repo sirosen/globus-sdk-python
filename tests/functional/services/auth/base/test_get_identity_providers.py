d0cc9359b722a320f161c867069e2f0f1a67b7e4
Stephen Rosen <sirosen@globus.org>
2023-06-21 15:26:40 -0500

    Test query_params on get_identity_providers

--- tests/functional/services/auth/base/test_get_identity_providers.py
+++ tests/functional/services/auth/base/test_get_identity_providers.py
@@ -28,3 +28,17 @@ def test_get_identity_providers_by_ids(client):
 def test_get_identity_providers_mutex_args(client):
     with pytest.raises(globus_sdk.GlobusSDKUsageError, match="mutually exclusive"):
         client.get_identity_providers(ids="foo", domains="bar")
+
+
+def test_get_identity_providers_allows_query_params_with_no_args(client):
+    # this test confirms that the request won't be rejected for passing arguments
+    # without specifying either 'ids' or 'domains' -- the supposition being that some
+    # other parameter is supported but unknown to the SDK
+    meta = load_response(client.get_identity_providers).metadata
+    res = client.get_identity_providers(query_params={"foo": "bar,baz,snork"})
+
+    assert [x["id"] for x in res] == meta["ids"]
+    assert [x for y in res for x in y["domains"]] == meta["domains"]
+
+    lastreq = get_last_request()
+    assert lastreq.params == {"foo": "bar,baz,snork"}
