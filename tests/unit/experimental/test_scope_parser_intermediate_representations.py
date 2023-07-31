d8759e2005d8033ed720b0cfa2ff0620707692ca
Stephen Rosen <sirosen@globus.org>
2023-07-31 14:58:33 -0500

    Make ScopeGraph __str__ tests less fragile
    These tests were sensitive to the exact whitespace generated by
    __str__, which is a cosmetic (non-semantic) detail.
    It is relatively simple to remove blank lines, which makes the tests
    more robust to any refinements we make in the future.

--- tests/unit/experimental/test_scope_parser_intermediate_representations.py
+++ tests/unit/experimental/test_scope_parser_intermediate_representations.py
@@ -6,63 +6,55 @@ from globus_sdk.experimental.scope_parser._parser import (
 
 def test_graph_str_single_node():
     g = parse_scope_graph("foo")
+    clean_str = _blank_lines_removed(str(g))
     assert (
-        str(g)
+        clean_str
         == """\
 digraph scopes {
   rankdir="LR";
-
   foo
-
-
 }"""
     )
 
 
 def test_graph_str_single_optional_node():
     g = parse_scope_graph("*foo")
+    clean_str = _blank_lines_removed(str(g))
     assert (
-        str(g)
+        clean_str
         == """\
 digraph scopes {
   rankdir="LR";
-
   *foo
-
-
 }"""
     )
 
 
 def test_graph_str_single_dependency():
     g = parse_scope_graph("foo[bar]")
+    clean_str = _blank_lines_removed(str(g))
     assert (
-        str(g)
+        clean_str
         == """\
 digraph scopes {
   rankdir="LR";
-
   foo
-
   foo -> bar;
-
 }"""
     )
 
 
 def test_graph_str_optional_dependency():
     g = parse_scope_graph("foo[bar[*baz]]")
+    clean_str = _blank_lines_removed(str(g))
     assert (
-        str(g)
+        clean_str
         == """\
 digraph scopes {
   rankdir="LR";
-
   foo
-
   foo -> bar;
   bar -> baz [ label = "optional" ];
-
 }"""
     )
 
@@ -77,3 +69,7 @@ def test_treenode_repr():
     t = ScopeTreeNode("foo", optional=False)
     t.dependencies = [ScopeTreeNode("bar", optional=False)]
     assert (repr(t)) == "ScopeTreeNode('foo', dependencies=[ScopeTreeNode('bar')])"
+
+
+def _blank_lines_removed(s: str) -> str:
+    return "\n".join(line for line in s.split("\n") if line.strip() != "")
