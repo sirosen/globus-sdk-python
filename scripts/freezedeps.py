41819f2e5443c7b9777edcb7bca48d8ff265d41e
Stephen Rosen <sirosen@globus.org>
2023-08-16 16:31:46 -0500

    Freeze 'mindeps' requirements with py3.7
    Rather than using the same (default) python for all of the
    requirements files in `requirements/*.in`, specify it more strictly
    thusly:
    
    - if `--mindeps` is passed, the freeze script will target the files
      with `mindeps` in their names
    - otherwise, the freeze script will skip files with `mindeps` in their
      names
    - `tox.ini` is extended to make `freezedeps` a label which runs
      `freezedeps` and `freezedeps-mindeps`
    - `freezedeps` has the same invocation path as today
    - `freezedeps-mindeps` uses py3.7, holds back the `pip` version to
      work around a pip-tools incompatibility which isn't ported back to
      the py3.7 pip-tools releases, and passes `--mindeps` to the script
    
    The end result is that we maintain *exactly* the same number of files
    as before, but now we use py3.7 for the 'mindeps' files and the user's
    current python (3.11 for me) for all of the others.

--- scripts/freezedeps.py
+++ scripts/freezedeps.py
@@ -6,6 +6,7 @@ tweaks/simplifications which are more appropriate to our usage.
 1. Run pip-compile independently for each X.in -> X.txt
    (pip-compile-multi cross-correlates between files)
 2. Set CUSTOM_COMPILE_COMMAND (pip-compile-multi does not support this)
+3. Use the '--mindeps' flag to control which requirements are compiled
 
 Equivalently, we could write out `pip-compile <options> <source> -o <dest>` in our
 tox.ini as the commands list.
@@ -18,11 +19,24 @@ import sys
 REPO_ROOT = pathlib.Path(__file__).parent.parent
 REQS_DIR = REPO_ROOT / "requirements"
 
+PY_MAJOR_MINOR = f"{sys.version_info[0]}{sys.version_info[1]}"
+
 
 def main():
+    try:
+        mindeps = sys.argv[1] == "--mindeps"
+    except IndexError:
+        mindeps = False
+
     reqs_in_files = REQS_DIR.glob("*.in")
-    print(f'running pip-compile on "{REQS_DIR}/*.in"')
+    print(f'running pip-compile on "{REQS_DIR}/*.in" (mindeps={mindeps})')
     for absolute_source in reqs_in_files:
+        # skip if it's a "mindeps" requirement and "mindeps" is not set
+        #         or if it's not a "mindeps" requirement and "mindeps" is set
+        if ("mindeps" not in absolute_source.name and mindeps) or (
+            "mindeps" in absolute_source.name and not mindeps
+        ):
+            continue
         source = absolute_source.relative_to(REQS_DIR)
         dest = source.with_suffix(".txt")
         print(f"{source} -> {dest}")
@@ -42,7 +56,7 @@ def main():
             ],
             check=True,
             cwd=REQS_DIR,
-            env={"CUSTOM_COMPILE_COMMAND": "tox run -e freezedeps"},
+            env={"CUSTOM_COMPILE_COMMAND": "tox p -m freezedeps"},
         )
 
 
