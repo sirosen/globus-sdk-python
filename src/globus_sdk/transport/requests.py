6de957433fe8dc883fa40a9127bb79516bc0c1be
Stephen Rosen <sirosen@globus.org>
2023-11-01 11:31:44 -0500

    Automatically convert UUID to str in requests
    The `RequestEncoder` classes define how we encode payloads and
    otherwise "prepare" a request. They are therefore a better location
    for any transforms we want to perform between some client call being
    made (i.e. `post()`, `get()`, ...) and the call to `requests`.
    
    Therefore, the change has two major pieces:
    - Move all transformation of request data from `BaseClient` and
      `PayloadWrapper`. This covers removal of MISSING from all three
      major locations for request data.
    - Add the capability to auto-stringify UUIDs to the encoder passes.
      (With the first part done, this is near-trivial.)
    
    Note that the names used in the encoders for these data are
    intentionally consistent with `requests.Request` fields as much as
    possible. So we have internal hooks for `_prepare_params`,
    `_prepare_headers`, and `_prepare_data` (that last one being the
    payload/body).
    
    Tests which were written for the PayloadWrapper have been replaced
    with tests for the encoders.
    
    `utils.filter_missing` is still defined separately because it was used
    in some tests to confirm the removal of `MISSING` from data -- it is
    therefore demonstrably useful independent from the other changes.
    That is why it is defined separately and used as part of the
    transformations, rather than being inlined there. (Inlining would make
    things marginally faster, but carries tradeoffs.)

--- src/globus_sdk/transport/requests.py
+++ src/globus_sdk/transport/requests.py
@@ -8,7 +8,7 @@ import typing as t
 
 import requests
 
-from globus_sdk import config, exc
+from globus_sdk import config, exc, utils
 from globus_sdk.authorizers import GlobusAuthorizer
 from globus_sdk.transport.encoders import (
     FormRequestEncoder,
@@ -274,7 +274,7 @@ class RequestsTransport:
         method: str,
         url: str,
         query_params: dict[str, t.Any] | None = None,
-        data: dict[str, t.Any] | str | None = None,
+        data: dict[str, t.Any] | utils.PayloadWrapper | str | None = None,
         headers: dict[str, str] | None = None,
         encoding: str | None = None,
         authorizer: GlobusAuthorizer | None = None,
