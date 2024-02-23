4565da418739064a5de53c942ef54f09a74c6559
Stephen Rosen <sirosen@globus.org>
2024-02-23 16:53:49 -0600

    Add exported symbol checker to pre-commit
    And fix the issues it flags. Document missing classes, and add a
    missing docstring to RemovedInV4Warning.

--- src/globus_sdk/exc/warnings.py
+++ src/globus_sdk/exc/warnings.py
@@ -4,7 +4,13 @@ import warnings
 
 
 class RemovedInV4Warning(DeprecationWarning):
-    pass
+    """
+    This warning indicates that a feature or usage was detected which will be
+    unsupported in globus-sdk version 4.
+
+    Users are encouraged to resolve these warnings when possible, so that they will be
+    able to upgrade to version 4 without issue.
+    """
 
 
 def warn_deprecated(message: str, stacklevel: int = 2) -> None:
