05556b6a790fdc402a232763c8391c8b23e2c756
Reid Mello <30907815+rjmello@users.noreply.github.com>
2024-10-25 15:54:13 -0400

    Add `ComputeClient` methods to submit & get tasks
    - Added the `.get_task()`, `.get_task_batch()`, `.get_task_group()`, and
    `.submit()` methods to the `ComputeClientV2` class.
    - Added the `.submit()` method to the `ComputeClientV3` class.

--- src/globus_sdk/_testing/data/compute/_common.py
+++ src/globus_sdk/_testing/data/compute/_common.py
@@ -1,5 +1,27 @@
 import uuid
 
+ENDPOINT_ID = str(uuid.uuid1())
+
 FUNCTION_ID = str(uuid.uuid1())
 FUNCTION_NAME = "howdy_world"
 FUNCTION_CODE = "410\n10\n04\n:gASVQAAAAAAAAACMC2hvd2R5X3dvc ..."
+
+TASK_GROUP_ID = str(uuid.uuid1())
+TASK_ID = str(uuid.uuid1())
+TASK_ID_2 = str(uuid.uuid1())
+TASK_DOC = {
+    "task_id": TASK_ID,
+    "status": "success",
+    "result": "10000",
+    "completion_t": "1677183605.212898",
+    "details": {
+        "os": "Linux-5.19.0-1025-aws-x86_64-with-glibc2.35",
+        "python_version": "3.10.4",
+        "dill_version": "0.3.5.1",
+        "globus_compute_sdk_version": "2.3.2",
+        "task_transitions": {
+            "execution-start": 1692742841.843334,
+            "execution-end": 1692742846.123456,
+        },
+    },
+}
---
+++ tests/functional/services/compute/v2/test_get_task_group.py
@@ -0,0 +1,11 @@
+import globus_sdk
+from globus_sdk._testing import load_response
+
+
+def test_get_task_group(compute_client_v2: globus_sdk.ComputeClientV2):
+    meta = load_response(compute_client_v2.get_task_group).metadata
+    res = compute_client_v2.get_task_group(task_group_id=meta["task_group_id"])
+    assert res.http_status == 200
+    assert meta["task_group_id"] == res.data["taskgroup_id"]
+    assert meta["task_id"] == res.data["tasks"][0]["id"]
+    assert meta["task_id_2"] == res.data["tasks"][1]["id"]
