d86e998ae71628bb29bd5dd0f0e4fa80adb38128
Stephen Rosen <sirosen@globus.org>
2024-12-15 13:53:57 -0600

    Add a new tox plugin setting to do `rm -r`
    Replace uses of `rm` in tox.ini with custom `rmtree` config setting,
    provided by the toxfile.py plugin.
    
    Advantages over using `rm`:
    - more concise, without the risk of forgetting `allowlist_externals`
    - more portable -- works on Windows
    
    Some projects have historically made themselves more portable by
    embedding `python -c ...` invocations in their tox config. This solves
    the problem of "how to do that cleanly".

--- toxfile.py
+++ toxfile.py
@@ -11,6 +11,7 @@ We only implement hooks which we need.
 
 from __future__ import annotations
 
+import logging
 import pathlib
 import shutil
 import typing as t
@@ -18,7 +19,11 @@ import typing as t
 from tox.plugin import impl
 
 if t.TYPE_CHECKING:
+    from tox.config.sets import EnvConfigSet
+    from tox.session.state import State
     from tox.tox_env.api import ToxEnv
+
+log = logging.getLogger(__name__)
 
 REPO_ROOT = pathlib.Path(__file__).parent
 BUILD_DIR = REPO_ROOT / "build"
@@ -33,3 +38,23 @@ def tox_on_install(
         return
     if BUILD_DIR.exists():
         shutil.rmtree(BUILD_DIR)
+
+
+@impl
+def tox_add_env_config(env_conf: EnvConfigSet, state: State) -> None:
+    env_conf.add_config(
+        keys=["globus_sdk_rmtree"],
+        of_type=list[str],
+        default=[],
+        desc="A dir tree to remove before running the environment commands",
+    )
+
+
+@impl
+def tox_before_run_commands(tox_env: ToxEnv) -> None:
+    sdk_rmtree = tox_env.conf.load("globus_sdk_rmtree")
+    for name in sdk_rmtree:
+        path = pathlib.Path(name)
+        if path.exists():
+            log.warning(f"globus_sdk_rmtree: {path}")
+            shutil.rmtree(path)
