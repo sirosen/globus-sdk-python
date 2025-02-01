9072635e94ed9098af6527e79f52cdcec8379a35
Stephen Rosen <sirosen@globus.org>
2025-01-31 18:29:18 -0600

    Move ExpandTestingFixture to new module and test
    Move this sphinx directive to a new dedicated module and add some
    basic unit tests for it.

--- src/globus_sdk/_sphinxext/custom_directives/__init__.py
+++ src/globus_sdk/_sphinxext/custom_directives/__init__.py
@@ -1,6 +1,7 @@
 from .add_content_directive import AddContentDirective
 from .automethodlist import AutoMethodList
 from .enumerate_testing_fixtures import EnumerateTestingFixtures
+from .expand_testing_fixture import ExpandTestingFixture
 from .externaldoclink import ExternalDocLink
 from .list_known_scopes import ListKnownScopes
 
@@ -8,6 +9,7 @@ __all__ = (
     "AddContentDirective",
     "AutoMethodList",
     "EnumerateTestingFixtures",
+    "ExpandTestingFixture",
     "ExternalDocLink",
     "ListKnownScopes",
 )
