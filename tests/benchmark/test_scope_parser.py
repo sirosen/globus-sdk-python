7ea88fa8698c7c5449015b951423df16ec20a453
Stephen Rosen <sirosen@globus.org>
2025-06-07 00:47:44 -0500

    Convert the parser perf test to pytest-benchmark
    This is an experiment in the use of `pytest-benchmark` to provide a
    structure for benchmarking in general. The perf test is moved and
    rephrased as a set of pytest test cases using the `benchmark` fixture.
    
    A new testenv, `tox r -e benchmark`, is defined to allow runs of these
    benchmarks.
    
    The `tests/benchmark/` dir is listed in norecursedirs, meaning that we
    don't need `pytest-benchmark` installed at all outside of the
    benchmark testing tox testenv.

---
+++ tests/benchmark/test_scope_parser.py
@@ -0,0 +1,32 @@
+import pytest
+
+from globus_sdk.scopes import Scope
+
+
+def _make_deep_scope(depth):
+    big_scope = ""
+    for i in range(depth):
+        big_scope += f"foo{i}["
+    big_scope += "bar"
+    for _ in range(depth):
+        big_scope += "]"
+    return big_scope
+
+
+def _make_wide_scope(width):
+    big_scope = ""
+    for i in range(width):
+        big_scope += f"foo{i} "
+    return big_scope
+
+
+@pytest.mark.parametrize("depth", (10, 100, 1000, 2000, 3000, 4000, 5000))
+def test_deep_scope_parsing(benchmark, depth):
+    scope_string = _make_deep_scope(depth)
+    benchmark(Scope.parse, scope_string)
+
+
+@pytest.mark.parametrize("width", (5000, 10000))
+def test_wide_scope_parsing(benchmark, width):
+    scope_string = _make_wide_scope(width)
+    benchmark(Scope.parse, scope_string)
