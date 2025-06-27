c73df2efa54bce64534950cb53da755d0672f1a6
Yadu Babuji <yadudoc1729@gmail.com>
2025-06-26 23:34:30 -0500

    Update `ComputeClientV2.get_endpoints` to list MEPs via a new `role` kwarg
    * Currently `get_endpoints` only queries for endpoints owned by the user.
      To list MEPs that are accessible but not owned by the user, the web-service supports a role query param that can be toggled from the
    default of "owner" to "any". Refer: https://compute.api.globus.org/docs#/Endpoints/get_endpoints_v2_endpoints_get
    * A new `role` kwarg is added to `ComputeClientV2.get_endpoints` that
      takes `roles="any"` to query for MEPs in addition to those owned by the user.

--- src/globus_sdk/services/compute/client.py
+++ src/globus_sdk/services/compute/client.py
@@ -6,6 +6,7 @@ import typing as t
 from globus_sdk import GlobusHTTPResponse, client, utils
 from globus_sdk._types import UUIDLike
 from globus_sdk.scopes import ComputeScopes, Scope
+from globus_sdk import MISSING, MissingType
 
 from .errors import ComputeAPIError
 
@@ -101,8 +102,10 @@ class ComputeClientV2(client.BaseClient):
         """  # noqa: E501
         return self.get(f"/v2/endpoints/{endpoint_id}/status")
 
-    def get_endpoints(self) -> GlobusHTTPResponse:
+    def get_endpoints(self, role: str | MissingType = MISSING) -> GlobusHTTPResponse:
         """Get a list of registered endpoints associated with the authenticated user.
+
+        :param role: Role of the user in relation to endpoints. (e.g.: owner, any)
 
         .. tab-set::
 
@@ -112,7 +115,8 @@ class ComputeClientV2(client.BaseClient):
                     :service: compute
                     :ref: Endpoints/operation/get_endpoints_v2_endpoints_get
         """  # noqa: E501
-        return self.get("/v2/endpoints")
+        query_params = {"role": role} if role else None
+        return self.get("/v2/endpoints", query_params=query_params)
 
     def delete_endpoint(self, endpoint_id: UUIDLike) -> GlobusHTTPResponse:
         """Delete a registered endpoint.
