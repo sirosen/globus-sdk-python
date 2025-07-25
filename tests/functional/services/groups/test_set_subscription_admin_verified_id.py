17df7f6d23aaed9707fb763402b66b7e697657f1
Charlie Whitehead <charlie@globus.org>
2025-07-25 16:04:27 -0400

    Use load_response instead of deprecated testing method

--- tests/functional/services/groups/test_set_subscription_admin_verified_id.py
+++ tests/functional/services/groups/test_set_subscription_admin_verified_id.py
@@ -1,21 +1,19 @@
-from tests.common import register_api_route_fixture_file
+import json
+
+from globus_sdk._testing import get_last_request, load_response
 
 
 def test_set_subscription_admin_verified_id(groups_client):
-    register_api_route_fixture_file(
-        "groups",
-        "/v2/groups/d3974728-6458-11e4-b72d-123139141556/subscription_admin_verified",
-        "set_subscription_admin_verified_id.json",
-        method="PUT",
-    )
+    meta = load_response(groups_client.set_subscription_admin_verified_id).metadata
 
     res = groups_client.set_subscription_admin_verified_id(
-        group_id="d3974728-6458-11e4-b72d-123139141556",
-        subscription_id="43db2f9f-601b-4c36-bbaa-26a97555935c",
+        group_id=meta["group_id"],
+        subscription_id=meta["subscription_id"],
     )
     assert res.http_status == 200
-    assert res.data["group_id"] == "d3974728-6458-11e4-b72d-123139141556"
-    assert (
-        res.data["subscription_admin_verified_id"]
-        == "43db2f9f-601b-4c36-bbaa-26a97555935c"
-    )
+    assert res.data["group_id"] == meta["group_id"]
+    assert res.data["subscription_admin_verified_id"] == meta["subscription_id"]
+
+    req = get_last_request()
+    req = json.loads(req.body)
+    assert req == {"subscription_admin_verified_id": meta["subscription_id"]}
